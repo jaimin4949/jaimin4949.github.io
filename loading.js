0901399f5b21efc74515071417f049e2f6f1ef8f
jaimin4949 <62239432+jaimin4949@users.noreply.github.com>
2021-07-19 18:41:06 +0530

    .

--- loading.js
+++ loading.js
@@ -1,1 +1,5 @@
+const loadingPage = document.getElementsByClassName('loading')[0];
 
+window.addEventListener('load', () => {
+    loadingPage.style.animation = 'loadingFade 0.5s ease-in 1 forwards';
+});
