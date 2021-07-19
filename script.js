7c96deabd5ed2b814931be999b0628cf87fd5851
jaimin4949 <62239432+jaimin4949@users.noreply.github.com>
2021-07-19 18:30:52 +0530

    .

--- script.js
+++ script.js
@@ -70,3 +70,9 @@ $(document).ready(function(){
         }
     });
 });
+
+const loadingPage = document.getElementsByClassName('loading')[0];
+
+window.addEventListener('load', () => {
+    loadingPage.style.animation = 'loadingFade 0.5s ease-in 1 forwards';
+});
