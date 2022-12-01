9bfb23289dbc5bfbdf929b562471ef127bb9fbdc
jaimin4949 <62239432+jaimin4949@users.noreply.github.com>
2022-12-01 05:45:04 +0530

    new
    nw

--- loading.js
+++ loading.js
@@ -1,5 +1,5 @@
-const loadingPage = document.getElementsByClassName('loading')[0];
+// const loadingPage = document.getElementsByClassName('loading')[0];
 
-window.addEventListener('load', () => {
-    loadingPage.style.animation = 'loadingFade 0.5s ease-in 1 forwards';
-});
+// window.addEventListener('load', () => {
+//     loadingPage.style.animation = 'loadingFade 0.5s ease-in 1 forwards';
+// });
